ad0d44b80385294fd13a381fd5c8d8b3b18fa15f
yue jia <jiayu@sheridancollege.ca>
2020-02-29 12:12:23 -0500

    Add some features

---
+++ YueJiaChallenge/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace YueJiaChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CLEAR = new System.Windows.Forms.Button();
+            this.Supp = new System.Windows.Forms.TextBox();
+            this.Cust = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.addchoi = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.addchoi);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.CLEAR);
+            this.panel1.Controls.Add(this.Supp);
+            this.panel1.Controls.Add(this.Cust);
+            this.panel1.Controls.Add(this.OK);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 700);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label2.Location = new System.Drawing.Point(167, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Supplier quantity";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(22, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Customer quantity";
+            // 
+            // CLEAR
+            // 
+            this.CLEAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CLEAR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CLEAR.Location = new System.Drawing.Point(170, 233);
+            this.CLEAR.Name = "CLEAR";
+            this.CLEAR.Size = new System.Drawing.Size(100, 50);
+            this.CLEAR.TabIndex = 3;
+            this.CLEAR.Text = "CLEAR";
+            this.CLEAR.UseVisualStyleBackColor = true;
+            this.CLEAR.Click += new System.EventHandler(this.CLEAR_Click);
+            // 
+            // Supp
+            // 
+            this.Supp.BackColor = System.Drawing.Color.DarkGray;
+            this.Supp.Location = new System.Drawing.Point(170, 138);
+            this.Supp.Name = "Supp";
+            this.Supp.Size = new System.Drawing.Size(100, 22);
+            this.Supp.TabIndex = 2;
+            // 
+            // Cust
+            // 
+            this.Cust.BackColor = System.Drawing.Color.DarkGray;
+            this.Cust.Location = new System.Drawing.Point(22, 138);
+            this.Cust.Name = "Cust";
+            this.Cust.Size = new System.Drawing.Size(100, 22);
+            this.Cust.TabIndex = 1;
+            // 
+            // OK
+            // 
+            this.OK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OK.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.OK.Location = new System.Drawing.Point(22, 233);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(100, 50);
+            this.OK.TabIndex = 0;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // addchoi
+            // 
+            this.addchoi.FormattingEnabled = true;
+            this.addchoi.Items.AddRange(new object[] {
+            "Add Customer",
+            "Add Supplier",
+            "Add Nothing"});
+            this.addchoi.Location = new System.Drawing.Point(25, 402);
+            this.addchoi.Name = "addchoi";
+            this.addchoi.Size = new System.Drawing.Size(121, 24);
+            this.addchoi.TabIndex = 6;
+            this.addchoi.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Adding  By Clicking On The Screen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1000, 700);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.Info;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.TextBox Cust;
+        private System.Windows.Forms.TextBox Supp;
+        private System.Windows.Forms.Button CLEAR;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox addchoi;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ YueJiaChallenge/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YueJiaChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private String addChoice="Add Nothing";
+        ArrayList s = new ArrayList();
+        ArrayList c = new ArrayList();
+        private Color co;
+        private Color[] colors =
+        {
+            Color.FromArgb(255,255,255,0),
+            Color.FromArgb(255,124,252,0),
+            Color.FromArgb(255,0,255,255),
+            Color.FromArgb(255,252,90,3),
+            Color.FromArgb(255,238,130,238),
+            Color.FromArgb(255,255,255,255)
+        };
+       
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        public void drawCustomer(float x, float y,Graphics g) {
+            Color col = Color.FromArgb(255,102,204,0);
+            Brush br = new SolidBrush(col);
+            g.FillEllipse(br, x-4, y-4, 9, 9);
+        }
+        public void drawSupplier(float x, float y, Graphics g)
+        {
+            Color col = Color.FromArgb(255, 255, 0, 127);
+            Brush br = new SolidBrush(col);
+            g.FillEllipse(br, x-4, y-4, 9, 9);
+        }
+        public ArrayList customer(int i,Graphics g) {
+            ArrayList cust = new ArrayList();
+            Random rd = new Random();
+            for (int j = 0; j < i; j++) {
+                Point pt = new Point(rd.Next(300,1000), rd.Next(0,700));
+                cust.Add(pt);
+                drawCustomer(pt.X,pt.Y,g);
+            }
+            return cust;
+        }
+        public ArrayList supplier(int i, Graphics g)
+        {
+            ArrayList supp = new ArrayList();
+            Random rd = new Random();
+            for (int j = 0; j < i; j++)
+            {
+                Point pt = new Point(rd.Next(350, 950), rd.Next(50, 650));
+                supp.Add(pt);
+                drawSupplier(pt.X, pt.Y, g);
+            }
+            return supp;
+        }
+
+        public void drawLine(Graphics g,Color color,float x1,float y1,float x2,float y2) {
+            Pen p = new Pen(color);
+            g.DrawLine(p,x1,y1,x2,y2);
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            s.Clear();
+            c.Clear();
+            //draw points
+            Graphics g = CreateGraphics();
+            try
+            {
+                 s = new ArrayList(supplier(Convert.ToInt32(Supp.Text), g));
+                 c = new ArrayList(customer(Convert.ToInt32(Cust.Text), g));
+                //draw line logic
+
+                float x = -50;
+                float y = -50;
+                for (int i = 0; i < c.Count; i++)
+                {
+                    double min = 999999;
+                    
+                    for (int j = 0; j < s.Count; j++)
+                    {
+                        double temp = Math.Sqrt(Math.Pow(((((Point)s[j]).X - ((Point)c[i]).X)), 2) + Math.Pow(((((Point)s[j]).Y - ((Point)c[i]).Y)), 2));
+                        if (min > temp)
+                        {
+                            min = temp;
+                            x = ((Point)s[j]).X;
+                            y = ((Point)s[j]).Y;
+                            co = colors[j % 6];
+                        }
+
+                    }
+                    drawLine(g, co, x, y, ((Point)c[i]).X, ((Point)c[i]).Y);
+                }
+            }
+            catch (Exception ee) {
+                MessageBox.Show(ee.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Paint += Form1_Paint;
+        }
+
+        private void CLEAR_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            s.Clear();
+            c.Clear();
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Refresh();
+            Graphics g = this.CreateGraphics();
+            //redraw points exist in the arraylist
+            foreach (Point p in s)
+            {
+                drawSupplier(p.X,p.Y,g);
+            }
+            foreach (Point p in c)
+            {
+                drawCustomer(p.X, p.Y, g);
+            }
+
+            int xx = e.X;
+            int yy = e.Y;
+
+            
+            Point pt = new Point(xx, yy);
+            switch (addChoice)
+            {
+                case "Add Customer":
+                    c.Add(pt);
+                    drawCustomer(xx, yy, g);
+                    break;
+                case "Add Supplier":
+                    s.Add(pt);
+                    drawSupplier(xx, yy, g);
+                    break;
+                default:
+                    break;
+            }
+            try { 
+            float x = -50;
+            float y = -50;
+            for (int i = 0; i < c.Count; i++)
+            {
+                double min = 999999;
+
+                for (int j = 0; j < s.Count; j++)
+                {
+                    double temp = Math.Sqrt(Math.Pow(((((Point)s[j]).X - ((Point)c[i]).X)), 2) + Math.Pow(((((Point)s[j]).Y - ((Point)c[i]).Y)), 2));
+                    if (min > temp)
+                    {
+                        min = temp;
+                        x = ((Point)s[j]).X;
+                        y = ((Point)s[j]).Y;
+                        co = colors[j % 6];
+                    }
+
+                }
+                drawLine(g, co, x, y, ((Point)c[i]).X, ((Point)c[i]).Y);
+            }
+        }
+            catch (Exception ee) {
+                MessageBox.Show(ee.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+}
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            addChoice = addchoi.Text;
+        }
+    }
+}
